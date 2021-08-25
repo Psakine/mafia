b9870b12c5e7c44a656aaac43c96b4e6a6c902f8
nikolai.kirdyashkin <nikolai.kirdyashkin@simbirsoft.com>
2021-08-25 09:27:06 +0300

    final changes

--- public/js/front.js
+++ public/js/front.js
@@ -1,70 +1,89 @@
-$(document).ready(function() {
-  var panel = $('.panel');
+$(document).ready(function () {
+    var panel = $('.panel');
+    var placeOut = [];
+    var renderCount = 0;
 
-  $(window).resize(function() {
-    $('.player-card').each(function(i, e) {
-      if ($(e).hasClass('out')) {
-        $(e).height($(e).width() / 1.248);
-      } else {
-        $(e).height($(e).width() / 0.624);
-      }
-    })
-  });
-  function getPlayers(){
-    $.ajax({
-      'url'      : '/api/games/last-players',
-      'method'   : 'get',
-      'dataType' : 'json'
-    }).done(function(result) {
-      panel.html('');
-      for (item in result) {
-        let card = result[item];
-        let out = '';
-        let killed = '';
-        let voted = '';
+    $(window).resize(function () {
+        $('.player-card').each(function (i, e) {
+            if ($(e).hasClass('out')) {
+                $(e).height($(e).width() / 1.248);
+            } else {
+                $(e).height($(e).width() / 0.624);
+            }
+        })
+    });
 
-        if(card.out == 'killed'){
-          out = ' out';
-          killed = 'killed'
-        }
-        if(card.out == 'voted'){
-          out = ' out';
-          voted = 'voted';
-        }
+    function getPlayers() {
+        $.ajax({
+            'url': '/api/games/last-players',
+            'method': 'get',
+            'dataType': 'json'
+        }).done(function (result) {
+            if (renderCount === 0) {
+                panel.html('');
+            }
+            for (item in result) {
+                let card = result[item];
+                let out = '';
+                let killed = '';
+                let voted = '';
 
-        let role = card.role ? card.role : '';
-        let nick = card.player.nickname;
-        let place = card.place;
-        let red = card.role == 'citizen' ? ' card-citizen' : '';
-        let photo =  './img/cat'+place+'.jpg';
+                if (card.out == 'killed') {
+                    out = ' out';
+                    killed = 'killed'
+                }
+                if (card.out == 'voted') {
+                    out = ' out';
+                    voted = 'voted';
+                }
 
-        if(card.player.photo_src !== null){
-          photo = card.player.photo_src;
-        }
+                let role = card.role ? card.role : '';
+                if (role == 'citizen') {
+                    role = '';
+                }
+                let nick = card.player.nickname;
+                let place = card.place;
+                let red = card.role == 'citizen' ? '' : '';
+                let photo = './img/cat' + place + '.jpg';
 
-        panel.append(
-          '<div class="player-card'+out+red+'" style="background-image: url('+photo+')">\n' +
-          '        <div class="player-card__header"><span class="number">'+place+'</span><span class="'+killed+voted+'"></div>\n' +
-          '        <div class="player-card__footer">' +
-          '        <span class="nickname">'+nick+'</span>' +
-          '        <span class="'+role+'"></span>' +
-          '     </div>\n' +
-          '    </div>');
-      }
-    })
-  }
-  function getGame(){
-    $.ajax({
-      'url'      : '/api/games/last-game',
-      'method'   : 'get',
-      'dataType' : 'json'
-    }).done(function(result) {
-      $('.game-name').html(result.name);
-    })
-  }
-  getGame();
-  getPlayers();
-  setInterval(function() {
+                if (card.player.photo_src !== null) {
+                    photo = card.player.photo_src;
+                }
+                if (renderCount === 0) {
+                    panel.append(
+                        '<div class="player-card' + out + red + '" style="background-image: url(' + photo + ')" id="place-' + place + '">' +
+                        // '<div class="player-card'+out+red+'" style="background-color: white">\n' +
+                        '        <div class="player-card__header"><span class="' + role + '"></span><span class="' + killed + voted + '"></div>' +
+                        '        <div class="player-card__footer">' + '<span class="number">' + place + '</span>' +
+                        '        <span class="nickname">' + nick + '</span>' +
+                        '     </div>\n' +
+                        '    </div>');
+                }
+                let player = $('#place-' + place);
+                if (out.length > 0 && typeof placeOut[place] == 'undefined') {
+                    placeOut[place] = place;
+                    $($('#place-' + place + ' span').get(1)).addClass(killed+voted)
+                    player.addClass('out');
+                    player.height('120px');
+                }
+            }
+
+            renderCount++
+        })
+    }
+
+    // function getGame(){
+    //   $.ajax({
+    //     'url'      : '/api/games/last-game',
+    //     'method'   : 'get',
+    //     'dataType' : 'json'
+    //   }).done(function(result) {
+    //     $('.game-name').html(result.name);
+    //   })
+    // }
+    // getGame();
     getPlayers();
-  }, 10000);
+    setInterval(function () {
+        getPlayers();
+    }, 10000);
 });
--- resources/views/front/game.blade.php
+++ resources/views/front/game.blade.php
@@ -2,64 +2,67 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+{{--    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">--}}
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=STIX+Two+Text&display=swap" rel="stylesheet">
     <link href="{{ asset('/css/all.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/fontawesome.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/front.css') }}" rel="stylesheet">
     <script src="{{ asset('/js/front.js') }}"></script>
 </head>
 <body>
-<div class="top-panel">
-    <span class="game-name">
-        Игра 0
-    </span>
-</div>
+{{--<body style="background: url('{{ asset('/Screenshot_2.png') }}'); width: 1920px; height: 1080px">--}}
+{{--<div class="top-panel">--}}
+{{--    <span class="game-name">--}}
+{{--        Игра 0--}}
+{{--    </span>--}}
+{{--</div>--}}
 
 <div class="panel">
-    <div class="player-card out" style="background-image: url('./img/cat1.jpg')">
-        <div class="player-card__header"><span class="sheriff"></span></div>
-        <div class="player-card__footer"><span class="number">1</span></div>
-    </div>
-    <div class="player-card" style="background-image: url('./img/cat2.jpg')">
-        <div class="player-card__header"></div>
-        <div class="player-card__footer"><span class="number">2</span><span class="club">rbf</span></div>
-    </div>
-    <div class="player-card" style="background-image: url('./img/cat3.jpg')">
-        <div class="player-card__header"></div>
-        <div class="player-card__footer"><span class="number">3</span><span class="club">om</span></div>
-    </div>
-    <div class="player-card out" style="background-image: url('./img/cat4.jpg')">
-        <div class="player-card__header"></div>
-        <div class="player-card__footer"><span class="number">4</span><span class="club">ds</span></div>
-    </div>
-    <div class="player-card" style="background-image: url('./img/cat5.jpg')">
-        <div class="player-card__header"></div>
-        <div class="player-card__footer"><span class="number">5</span><span class="club">sher</span></div>
-    </div>
-    <div class="player-card out" style="background-image: url('./img/cat6.jpg')">
-        <div class="player-card__header"></div>
-        <div class="player-card__footer"><span class="number">6</span><span class="club">om</span></div>
-    </div>
-    <div class="player-card" style="background-image: url('./img/cat7.jpg')">
-        <div class="player-card__header"></div>
-        <div class="player-card__footer"><span class="number">7</span><span class="club">om</span></div>
-    </div>
-    <div class="player-card" style="background-image: url('./img/cat8.jpg')">
-        <div class="player-card__header"><span class="role"><i class="fas fa-bahai"></i></span></div>
-        <div class="player-card__footer"><span class="number">8</span><span class="club">om</span></div>
-    </div>
-    <div class="player-card" style="background-image: url('./img/cat9.jpg')">
-        <div class="player-card__header"></div>
-        <div class="player-card__footer"><span class="number">9</span><span class="club">om</span></div>
-    </div>
-    <div class="player-card" style="background-image: url('./img/cat10.jpg')">
-        <div class="player-card__header"></div>
-        <div class="player-card__footer"><span class="number">10</span><span class="club">om</span></div>
-    </div>
+{{--    <div class="player-card out" style="background-image: url('./img/cat1.jpg')">--}}
+{{--        <div class="player-card__header"><span class="sheriff"></span></div>--}}
+{{--        <div class="player-card__footer"><span class="number">1</span></div>--}}
+{{--    </div>--}}
+{{--    <div class="player-card" style="background-image: url('./img/cat2.jpg')">--}}
+{{--        <div class="player-card__header"></div>--}}
+{{--        <div class="player-card__footer"><span class="number">2</span><span class="club">rbf</span></div>--}}
+{{--    </div>--}}
+{{--    <div class="player-card" style="background-image: url('./img/cat3.jpg')">--}}
+{{--        <div class="player-card__header"></div>--}}
+{{--        <div class="player-card__footer"><span class="number">3</span><span class="club">om</span></div>--}}
+{{--    </div>--}}
+{{--    <div class="player-card out" style="background-image: url('./img/cat4.jpg')">--}}
+{{--        <div class="player-card__header"></div>--}}
+{{--        <div class="player-card__footer"><span class="number">4</span><span class="club">ds</span></div>--}}
+{{--    </div>--}}
+{{--    <div class="player-card" style="background-image: url('./img/cat5.jpg')">--}}
+{{--        <div class="player-card__header"></div>--}}
+{{--        <div class="player-card__footer"><span class="number">5</span><span class="club">sher</span></div>--}}
+{{--    </div>--}}
+{{--    <div class="player-card out" style="background-image: url('./img/cat6.jpg')">--}}
+{{--        <div class="player-card__header"></div>--}}
+{{--        <div class="player-card__footer"><span class="number">6</span><span class="club">om</span></div>--}}
+{{--    </div>--}}
+{{--    <div class="player-card" style="background-image: url('./img/cat7.jpg')">--}}
+{{--        <div class="player-card__header"></div>--}}
+{{--        <div class="player-card__footer"><span class="number">7</span><span class="club">om</span></div>--}}
+{{--    </div>--}}
+{{--    <div class="player-card" style="background-image: url('./img/cat8.jpg')">--}}
+{{--        <div class="player-card__header"><span class="role"><i class="fas fa-bahai"></i></span></div>--}}
+{{--        <div class="player-card__footer"><span class="number">8</span><span class="club">om</span></div>--}}
+{{--    </div>--}}
+{{--    <div class="player-card" style="background-image: url('./img/cat9.jpg')">--}}
+{{--        <div class="player-card__header"></div>--}}
+{{--        <div class="player-card__footer"><span class="number">9</span><span class="club">om</span></div>--}}
+{{--    </div>--}}
+{{--    <div class="player-card" style="background-image: url('./img/cat10.jpg')">--}}
+{{--        <div class="player-card__header"></div>--}}
+{{--        <div class="player-card__footer"><span class="number">10</span><span class="club">om</span></div>--}}
+{{--    </div>--}}
 </div>
 </body>
 </html>
